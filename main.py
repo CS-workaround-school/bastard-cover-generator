1e692da83b3015de755ff4ceb7de8d4392529094
CS-workaround-school <rizzgod046@gmail.com>
2025-04-09 16:07:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ st.title("BASTARD Style Cover Generator")
 
 uploaded_image = st.file_uploader("Upload a background image (jpg, png)", type=["jpg", "jpeg", "png"])
 
-if uploaded_image is not None:
-  img = Image.open(uploaded_image)
-  st.image(img, caption="Uploaded Image", use_column_width=True)
-
 custom_text = st.text_input("Type your custom word or phrase", value="bastard")
 
 FONT_PATH = "fonts/CooperBlack.ttf"
@@ -19,19 +15,20 @@ def add_bastard_text(img, text, font_path):
   img = img.convert("RGB")
   img = img.resize((800, 800))
 
-draw = ImageDraw.Draw(img)
-font = ImageFont.truetype(font_path, 120)
+  draw = ImageDraw.Draw(img)
+  font = ImageFont.truetype(font_path, 120)
 
-text = text.upper()
-text_width, text_height = draw.textsize(text, font=font)
-x = (img.width - text_width) // 2
-y = (img.height - text_height) // 2
+  text = text.upper()
+  text_width, text_height = draw.textsize(text, font=font)
+  x = (img.width - text_width) // 2
+  y = (img.height - text_height) // 2
 
-draw.text((x, y), text, font=font, fill=(0, 255, 255))
+  draw.text((x, y), text, font=font, fill=(0, 255, 255))
 
-return img
+  return img
 
 if uploaded_image and custom_text:
+  
   img = Image.open(uploaded_image)
 
   final_img = add_bastard_text(img, custom_text, FONT_PATH)
